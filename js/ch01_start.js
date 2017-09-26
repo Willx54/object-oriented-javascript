a142aef87c854b6c67e15835c7c3624d69bada76
Will Priddle <will.priddle@nelsoncroom.co.uk>
2017-09-26 09:42:03 +0100

    clock exercise

--- js/ch01_start.js
+++ js/ch01_start.js
@@ -6,6 +6,10 @@ function onReady() {
 	console.log(date.getHours());
 	console.log(date.getMinutes());
 	console.log(date.getSeconds());
+
+	var clock = document.getElementById('clock');
+
+	clock.innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
 
 window.onload = onReady;

8681d36656190db5e1fa37e716287db9563e3739
Will Priddle <will.priddle@nelsoncroom.co.uk>
2017-09-26 09:34:35 +0100

    first commit

---
+++ js/ch01_start.js
@@ -0,0 +1,14 @@
+function onReady() {
+	console.log('Hello Chapter 1');
+
+	var date = new Date();
+
+	console.log(date.getHours());
+	console.log(date.getMinutes());
+	console.log(date.getSeconds());
+}
+
+window.onload = onReady;
+
+//follow me at https://twitter.com/02geek
+//learn more about me at http://02geek.com
